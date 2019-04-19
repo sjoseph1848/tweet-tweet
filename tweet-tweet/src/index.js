85801f6ce2d222563de93e40e5fde745505bd2c8
Samuel <sjoseph1848@gmail.com>
2019-04-19 07:49:37 -0500

    added message and styling

--- tweet-tweet/src/index.js
+++ tweet-tweet/src/index.js
@@ -5,8 +5,43 @@ import './index.css';
 function Tweet() {
   return(
     <div className="tweet">
-      Tweet
+      <Avatar />
+      <div className="conten">
+        <NameWithHandle/>
+        <Message />
+     </div>
     </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+    src="https://source.unsplash.com/user/erondu/daily"
+    className="avatar"
+    alt="avatar" />
+  );
+}
+
+function Message() {
+  return (
+    <div className="message">
+      Blah Blah Blah
+    </div>
+  );
+}
+
+function NameWithHandle() {
+  return (
+    <span 
+      className="name-with-handle">
+      <span className="name">
+        Rob Johnson 
+      </span>
+      <span className="handle">
+        @robjohnson123
+      </span>
+    </span>
   );
 }
 
