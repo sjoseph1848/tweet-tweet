85f8436d059608ce7674c5a7a96cf0f170571618
Samuel <sjoseph1848@gmail.com>
2019-04-19 08:00:30 -0500

    Added buttons and styling

--- tweet-tweet/src/index.js
+++ tweet-tweet/src/index.js
@@ -9,10 +9,18 @@ function Tweet() {
       <div className="conten">
         <NameWithHandle/>
         <Message />
+        <div className="buttons">
+          <ReplyButton/>
+          <RetweetButton/>
+          <LikeButton />
+          <MoreOptionsButton />        
+        </div>
      </div>
     </div>
   );
 }
+
+
 
 function Avatar() {
   return (
@@ -44,6 +52,29 @@ function NameWithHandle() {
     </span>
   );
 }
+
+
+//Buttons 
+const Time = () => 
+  <span className="time">
+    3h ago
+  </span>;
+
+
+const ReplyButton = () => 
+  <i className=" fa fa-reply \ 
+    reply-button" />
+
+const RetweetButton = () => 
+  <i className="fa fa-retweet \
+    retweet-button" />;
+
+const LikeButton = () => 
+  <i className="fa fa-heart \
+      like-button" />;
+const MoreOptionsButton = () => 
+  <i className="fa fa-ellipsis-h \
+    more-options-buttons" />;
 
 ReactDOM.render(
   <Tweet/>,
